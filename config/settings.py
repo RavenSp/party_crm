bef092c1953cf6aa938912ef47fbd2ed7c826da4
RavenSp <sp.raven@yandex.ru>
2024-02-24 22:52:56 +0300

    add static

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-r5o@37w2#4)(_fl29z2f%z&2dg#c!337tlmhuk@p_s2+gpoj^v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+BASE_URL = 'http://localhost:8000'
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["https://*", "http://*", "https://firm-oddly-husky.ngrok-free.app"]
+
 
 
 # Application definition
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')s
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

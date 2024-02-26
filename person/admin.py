a574ca1c3d3f59534561ce69a7708c958601f99b
RavenSp <sp.raven@yandex.ru>
2024-02-26 23:54:13 +0300

    add new form and admin gor person

--- person/admin.py
+++ person/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import Person
+
+
+class PersonAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = Person
+    list_display = ('email', 'is_staff', 'is_active', 'party_member')
+    list_filter = ('email', 'is_staff', 'is_active', 'party_member')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.register(Person, PersonAdmin)

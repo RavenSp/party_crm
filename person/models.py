6bc44dfd14b288387dd6e901b8002b559554fb41
RavenSp <sp.raven@yandex.ru>
2024-02-27 21:56:50 +0300

    add person and press models

--- person/models.py
+++ person/models.py
@@ -5,6 +5,13 @@ from django.utils.translation import gettext_lazy as _
 from .managers import CustomUserManager
 
 
+class PartyOrganization(models.Model):
+    title = models.CharField(verbose_name='Наименование парт. организации', max_length=255)
+
+    def __str__(self):
+        return str(self.title)
+
+
 class Person(AbstractUser):
     username = None
     email = models.EmailField(_("email address"), unique=True)
@@ -12,8 +19,7 @@ class Person(AbstractUser):
     bio = models.TextField(verbose_name='Биография', blank=True, null=True)
     party_member = models.BooleanField(verbose_name='Член партии', default=True)
     party_ticket_number = models.CharField(verbose_name='Номер партбилета', max_length=255, blank=True, null=True)
-
-
+    party_organization = models.ForeignKey(to=PartyOrganization, verbose_name='Партйиная организация', blank=True, null=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
@@ -22,4 +28,9 @@ class Person(AbstractUser):
 
     def __str__(self):
         return self.email
+
+    def set_party_organization(self, party_organization: PartyOrganization):
+        if not self.party_member:
+            raise ValueError('Чтобы добавить человека в партийную организацию он должен быть челном партии!')
+        self.party_organization = party_organization
 
--- press/models.py
+++ press/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from person.models import Person
 
-# Create your models here.
+class Newspaper(models.Model):
+    title = models.CharField(verbose_name='Название газеты', max_length=255)
+
+    def __str__(self):
+        return str(self.title)
+
+    class Meta:
+        verbose_name = 'Газета'
+        verbose_name_plural = 'Газеты'
+
+
+class NewspaperNumber(models.Model):
+    newspaper = models.ForeignKey(to=Newspaper, verbose_name='Газета')
+    number = models.CharField(verbose_name='Номер', max_length=255)
+    year = models.DateField(verbose_name='Год и месяц выхода', null=True, blank=True)
+
+    # Вероятно сюда можно будет потом добавить и файл газеты
+
+    class Meta:
+        verbose_name = 'Номер газеты'
+        verbose_name_plural = 'Номера газет'
+
+    def __str__(self):
+        return f"{self.newspaper.title} {self.number}"
+
+
+class Town(models.Model):
+    title = models.CharField(verbose_name='Город', max_length=255)
+
+    def __str__(self):
+        return str(self.title)
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+
+
+class Factory(models.Model):
+    title = models.CharField(verbose_name='Наименование предприятия', max_length=255)
+    description = models.TextField(blank=True, null=True, verbose_name='Примечание')
+    town = models.ForeignKey(to=Town, verbose_name='Город расположения', related_name='factories')
+
+    def __str__(self):
+        return str(self.title)
+
+    class Meta:
+        verbose_name = 'Предприятие'
+        verbose_name_plural = 'Предприятия'
+
+
+class Distribution(models.Model):
+    distribution_date = models.DateField(verbose_name='Дата раздачи')
+    autor = models.ForeignKey(to=Person, verbose_name='Автор записи')
+    factory = models.ForeignKey(to=Factory, verbose_name='Предприятие')
+    start_time = models.TimeField(verbose_name='Начало раздачи')
+    end_time = models.TimeField(verbose_name='Окончание раздачи')
+    description = models.TextField(blank=True, null=True, verbose_name='Примечание')
+
+
+class NewspaperNumbersOnDistribution(models.Model):
+    number = models.ForeignKey(to=NewspaperNumber, verbose_name='Номер газеты')
+    quantity = models.IntegerField(verbose_name='Количество')
+
+

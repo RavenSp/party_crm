4e17feee069614212d06c4123f960e9d8beb60b7
RavenSp <sp.raven@yandex.ru>
2024-03-17 12:22:00 +0300

    add bootstrap select 2

--- press/urls.py
+++ press/urls.py
@@ -1,6 +1,7 @@
-
 from django.urls import path
-from .views import my_distribution
+from .views import my_distribution, new_distrib
+
 urlpatterns = [
-    path('', my_distribution, name='all')
+    path('', my_distribution, name='all'),
+    path('new-distrib/', new_distrib, name='new-distrib')
 ]
--- press/views.py
+++ press/views.py
@@ -3,14 +3,29 @@ import datetime
 from django.shortcuts import render
 from django.http import HttpRequest
 from .forms import DistributionForm
-from .models import FactoryPoint
+from .models import FactoryPoint, Sympathizer
+from person.models import Person
 # Create your views here.
 
 def my_distribution(request: HttpRequest):
-    factories = FactoryPoint.objects.filter()
-    form = DistributionForm(initial={
-        'distribution_date': datetime.date.today().strftime('%Y-%m-%d'),
-        'start_time': datetime.datetime.now().strftime("%H:%M"),
-        'end_time': (datetime.datetime.now() + datetime.timedelta(minutes=60)).strftime("%H:%M"),
-    })
-    return render(request, 'press/all_distribution.html', {'form': form})
+    if request.method == 'GET':
+        return render(request, 'press/all_distribution.html', {})
+
+
+def new_distrib(request: HttpRequest):
+    if request.method == 'GET':
+        form = DistributionForm(initial={
+            'distribution_date': datetime.date.today().strftime('%Y-%m-%d'),
+            'start_time': (datetime.datetime.now() - datetime.timedelta(minutes=60)).strftime("%H:%M"),
+            'end_time': datetime.datetime.now().strftime("%H:%M"),
+        })
+
+        party_members = Person.objects.filter(party_member=True).order_by('last_name').all()
+        sympathizers = Sympathizer.objects.all()
+        return render(request, 'press/new-distrib.html', {
+            'form': form,
+            'party_members': party_members,
+            'sympathizers': sympathizers
+        })
+    elif request.method == 'POST':
+        pass

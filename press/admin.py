7445be9ad18a3131aaa05334791891d39e49854f
Maxim.Balashov <m.a.balashov@yandex.ru>
2024-05-27 22:51:07 +0300

    Настройка админ меню.

--- press/admin.py
+++ press/admin.py
@@ -2,12 +2,40 @@ from django.contrib import admin
 from .models import Newspaper, NewspaperNumber, Town, FactoryPoint, Distribution, Sympathizer, NewspaperNumbersOnDistribution, DistributionPartyMembers, DistributionSympathizerMember
 # Register your models here.
 
+
+class NewspaperNumbersOnDistributionInline(admin.StackedInline):
+    model = NewspaperNumbersOnDistribution
+    extra = 1
+    can_delete = True
+    verbose_name = 'Номер газеты'
+    verbose_name_plural = 'Номера газет'
+
+
+class DistributionPartyMembersInline(admin.StackedInline):
+    model = DistributionPartyMembers
+    extra = 1
+    can_delete = True
+    verbose_name = 'Член партии'
+    verbose_name_plural = 'Члены партии'
+
+
+class DistributionSympathizerMemberInline(admin.StackedInline):
+    model = DistributionSympathizerMember
+    extra = 1
+    can_delete = True
+    verbose_name = 'Сочувствующий'
+    verbose_name_plural = 'Сочувствующие'
+
+class DistributionSympathizerMemberAdmin(admin.ModelAdmin):
+    model = DistributionSympathizerMember
+    inlines = [NewspaperNumbersOnDistributionInline, DistributionPartyMembersInline, DistributionSympathizerMemberInline]
+
 admin.site.register(Newspaper)
 admin.site.register(NewspaperNumber)
 admin.site.register(Town)
 admin.site.register(FactoryPoint)
-admin.site.register(Distribution)
+admin.site.register(Distribution, DistributionSympathizerMemberAdmin)
 admin.site.register(Sympathizer)
-admin.site.register(NewspaperNumbersOnDistribution)
-admin.site.register(DistributionPartyMembers)
-admin.site.register(DistributionSympathizerMember)
+# admin.site.register(NewspaperNumbersOnDistribution)
+# admin.site.register(DistributionPartyMembers)
+# admin.site.register(DistributionSympathizerMember)

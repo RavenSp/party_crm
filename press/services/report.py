9777d457234e923e3f81ed2f7c0d999a06a9d1a9
RavenSp <sp.raven@yandex.ru>
2024-06-12 21:36:58 +0300

    Продолжаю работать над генерацией отчёта

--- press/services/report.py
+++ press/services/report.py
@@ -13,11 +13,11 @@ def generate_report():
     all_distribs = Distribution.objects.filter(
         distribution_date__gte=report_month.replace(month=1, day=1)
     ).filter(
-        distribution_date__lt=(report_month.replace(month=report_month.month+1) if report_month.month > 12 else report_month.replace(year=report_month.year +1, month=1, day=1))
-    ).prefetch_related('numbers')\
-    .prefetch_related('newspaper_numbers').all()
+        distribution_date__lt=(report_month.replace(month=report_month.month+1) if report_month.month < 12 else report_month.replace(year=report_month.year +1, month=1, day=1))
+    ).all()
+    
 
-    print(all_distribs.query)
+    # print(all_distribs.query)
 
     bytesFile = BytesIO()
 
@@ -26,21 +26,76 @@ def generate_report():
         'bold': 1,
         'align': 'center',
         'valign': 'vcenter',
-        'font_size': 16
+        'font_size': 16,
+        'font_name': 'Arial',
+    })
+
+    head_style = xls_file.add_format({
+        'bold': 1,
+        'align': 'center',
+        'valign': 'vcenter',
+        'font_size': 14,
+        'font_name': 'Arial',
+    })
+
+    simple_style = xls_file.add_format({
+        'align': 'center',
+        'valign': 'vcenter',
+        'font_size': 11,
+        'font_name': 'Arial',
+    })
+    xls_file.set_properties({
+        'title': 'Отчёт о раздачах газет Московской организации РПР',
+        'subject': 'Отчёт от раздаче',
+        'author': 'Report Maker',
+        'created': datetime.date.today(),
     })
     ws1 = xls_file.add_worksheet('Общие данные')
+    
     ws1.set_column('A:A', 15)
     ws1.set_column('B:B', 37.4)
     ws1.set_column('C:C', 33.3)
     ws1.set_column('D:D', 17.2)
     ws1.set_column('E:E', 35)
     ws1.merge_range('A1:E1', f"Распространение прессы Московская организация {datetime.date.today().year} год.", title_style)
-
+    ws1.set_row(0, 20)
+    ws1.set_row(1, 20)
+    for index, cell in enumerate(['Дата', 'Предприятие', 'Газета', 'Количество', 'Распространяли']):
+        ws1.write(1, index, cell, head_style)
+        for index, distrib in enumerate(all_distribs):
+            current_line = index+2
+            ws1.write_datetime(1, current_line, distrib.distribution_date, simple_style)
+            ws1.write_string(2, current_line, distrib.factory.title)
+            if distrib.numbers.count() > 1:
+                pass
+            else:
+                ws1.write_string(3, current_line, distrib.numbers.all()[0].number.newspaper.title, simple_style)
+    
     ws2 = xls_file.add_worksheet('Распространители')
-    ws2.merge_range('A1:O1', f'Распространители Московская организация {datetime.date.today().year} год.', title_style)
-    ws3 = xls_file.add_worksheet('Предприятия')
-    ws3.merge_range('A1:O1', f'Предприятия Московская организация {datetime.date.today().year} год.', title_style)
+    ws2.set_column('A:A', 28.05)
+    ws2.set_column('B:N', 13.35)
+    ws2.merge_range('A1:N1', f'Распространители Московская организация {datetime.date.today().year} год.', title_style)
+    ws2.set_row(0, 20)
+    ws2.set_row(1, 20)
+    row_text = ['Фамилия', 'Январь', 'Февраль', 'Март', 'Апрель',
+                'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь',
+                'Октябрь', 'Ноябрь', 'Декабрь', 'Итого:']
+    for index, cell in enumerate(row_text):
+        ws2.write(1, index, cell, head_style)
 
+
+    ws3 = xls_file.add_worksheet('Предприятия')
+    ws3.set_column('A:A', 40)
+    ws3.set_column('B:B', 30)
+    ws3.set_column('C:O', 13.35)
+    ws3.merge_range('A1:O1', f'Предприятия Московская организация {datetime.date.today().year} год.', title_style)
+    ws3.set_row(0, 20)
+    ws3.set_row(1, 20)
+    row_text = ['Предприятие', 'Город/Населённый пункт', 'Январь', 'Февраль', 'Март', 'Апрель',
+                'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь',
+                'Октябрь', 'Ноябрь', 'Декабрь', 'Итого:']
+    for index, cell in enumerate(row_text):
+        ws3.write(1, index, cell, head_style)
 
     xls_file.close()
     bytesFile.seek(0)

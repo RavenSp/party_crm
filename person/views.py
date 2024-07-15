5b43356410c57e0c99db2f4b793be57679c5238f
max_balashov <maxim.balashov@arora.pro>
2024-07-15 16:00:54 +0300

    Добавил корректную обработку статистики для сочувствующих

--- person/views.py
+++ person/views.py
@@ -2,7 +2,7 @@ from django.db.models import Sum
 from django.shortcuts import render, redirect
 from django.http import HttpRequest
 from person.services.login import auth_user
-from press.models import DistributionPartyMembers, Distribution
+from press.models import DistributionPartyMembers, Sympathizer, DistributionSympathizerMember
 import datetime
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
@@ -22,17 +22,29 @@ def login(request: HttpRequest):
 
 @login_required()
 def profile(request: HttpRequest):
-    my_distribution = DistributionPartyMembers.objects.select_related('distribution') \
-        .filter(member_id=request.user.pk) \
-        .filter(distribution__distribution_date__month=datetime.date.today().month) \
-        .filter(distribution__distribution_date__year=datetime.date.today().year) \
-        .order_by('distribution__distribution_date')\
-        .all()
-    all_distribution = DistributionPartyMembers.objects.select_related('distribution') \
-        .filter(member_id=request.user.pk) \
-        .order_by('distribution__distribution_date') \
-        .all()
-
+    if request.user.party_member:
+        my_distribution = DistributionPartyMembers.objects.select_related('distribution') \
+            .filter(member_id=request.user.pk) \
+            .filter(distribution__distribution_date__month=datetime.date.today().month) \
+            .filter(distribution__distribution_date__year=datetime.date.today().year) \
+            .order_by('distribution__distribution_date')\
+            .all()
+        all_distribution = DistributionPartyMembers.objects.select_related('distribution') \
+            .filter(member_id=request.user.pk) \
+            .order_by('distribution__distribution_date') \
+            .all()
+    else:
+        sympathizer = Sympathizer.objects.filter(name=f"{request.user.last_name} {request.user.first_name}").first()
+        my_distribution = DistributionSympathizerMember.objects.select_related('distribution') \
+            .filter(member_id=sympathizer.pk) \
+            .filter(distribution__distribution_date__month=datetime.date.today().month) \
+            .filter(distribution__distribution_date__year=datetime.date.today().year) \
+            .order_by('distribution__distribution_date') \
+            .all()
+        all_distribution = DistributionSympathizerMember.objects.select_related('distribution') \
+            .filter(member_id=sympathizer.pk) \
+            .order_by('distribution__distribution_date') \
+            .all()
     sum_my_distrib = sum([x.quantity for x in my_distribution if x.quantity])
     sum_all_distrib = sum([x.quantity for x in all_distribution if x.quantity])
 
